e20081c00f18e75108b1657c7cff0271b368f041
dvhar <dh22@protonmail.com>
2026-01-07 22:29:30 -0600

    update config style

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
+#pragma once
 #define COLOR(hex)    { ((hex >> 16) & 0xFF) / 255.0f, \
                         ((hex >> 8) & 0xFF) / 255.0f, \
                         (hex & 0xFF) / 255.0f }
 
-const int button_height = 30;
-const int button_spacing = 3;
-const int text_padding = 20;
-const int min_width = 100;
-const int separator_size = 1;
+static const int button_height = 30;
+static const int button_spacing = 3;
+static const int text_padding = 20;
+static const int min_width = 100;
+static const int separator_size = 1;
 
-const double button_grad_left[3]  = {0.22, 0.13, 0.10}; // dark blue-ish
-const double button_grad_right[3] = {0.32, 0.20, 0.18}; // lighter blue-ish
-const double hovered_grad_left[3]  = {0.43, 0.30, 0.26};
-const double hovered_grad_right[3] = {0.57, 0.43, 0.38};
+static const float button_grad_left[3]  = COLOR(0x38211a); // #38211a
+static const float button_grad_right[3] = COLOR(0x52332e); // #52332e
+static const float hovered_grad_left[3]  = COLOR(0x6e4c42); // #6e4c42
+static const float hovered_grad_right[3] = COLOR(0x916e61); // #916e61
+static const float sep_color[] = COLOR(0xf09000); // #f09000
+static const float menu_back[] = COLOR(0x0a0900); // #0a0900
+static const float text_color[] = COLOR(0xf0f0f0); // #f0f0f0
+static const float border_color[] = COLOR(0x505055); // #505055
 
-const float sep_color[] = COLOR(0xf09000);
-const float menu_back[] = COLOR(0x0a0900);
-const float text_color[] = COLOR(0xf0f0f0);
-const float border_color[] = COLOR(0x505055);
 
-
-const char* const font  = "Sans 12";
+static const char* const font  = "Sans 12";
