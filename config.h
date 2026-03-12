376d8285020cf7673d68271ff50b9c9c48c4a864
dvhar <dh22@protonmail.com>
2026-03-11 20:24:29 -0500

    default grey

--- config.h
+++ config.h
@@ -9,10 +9,10 @@ static const int text_padding = 20;
 static const int min_width = 100;
 static const int separator_size = 1;
 
-static const float button_grad_left[3]  = COLOR(0x38211a); // #38211a
-static const float button_grad_right[3] = COLOR(0x52332e); // #52332e
-static const float hovered_grad_left[3]  = COLOR(0x6e4c42); // #6e4c42
-static const float hovered_grad_right[3] = COLOR(0x916e61); // #916e61
+static const float button_grad_left[3]  = COLOR(0x303030); // #303030
+static const float button_grad_right[3] = COLOR(0x303030); // #404040
+static const float hovered_grad_left[3]  = COLOR(0x505050); // #505050
+static const float hovered_grad_right[3] = COLOR(0x505050); // #606060
 static const float sep_color[] = COLOR(0xf09000); // #f09000
 static const float menu_back[] = COLOR(0x0a0900); // #0a0900
 static const float text_color[] = COLOR(0xf0f0f0); // #f0f0f0
